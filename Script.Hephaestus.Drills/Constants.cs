97a03ff0ca6762d869ccb47fc6a4b0527cf90bf8
Alex Davidson <alex.davidson@bluewire-technologies.com>
2020-09-27 16:51:55 +0100

    Adjust drill speeds

--- Script.Hephaestus.Drills/Constants.cs
+++ Script.Hephaestus.Drills/Constants.cs
@@ -2,8 +2,8 @@
 {
     public static partial class Constants
     {
-        public const float DRILL_ROTATION_SPEED_RPM = 1f;
-        public const float DRILL_EXTENSION_SPEED_METRES_PER_SECOND = 0.2f;
-        public const float DRILL_RETRACTION_SPEED_METRES_PER_SECOND = 0.3f;
+        public const float DRILL_ROTATION_SPEED_RPM = 0.8f;
+        public const float DRILL_EXTENSION_SPEED_METRES_PER_SECOND = 0.11f;
+        public const float DRILL_RETRACTION_SPEED_METRES_PER_SECOND = 0.25f;
     }
 }
